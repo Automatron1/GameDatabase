8a52241fd0bfbcdfad84e46041e0993923d71bb4
Danny Farage <faraged001@mymail.mssu.edu>
2022-03-18 19:11:39 -0500

    YO

--- GameDatabase/MainWindow.xaml.cs
+++ GameDatabase/MainWindow.xaml.cs
@@ -21,32 +21,22 @@ namespace GameDatabase
    /// </summary>
    public partial class MainWindow : Window
    {
+      public static MainWindow Current;
       public MainWindow()
       {
          InitializeComponent();
+         Current = this;
       }
 
       private void Button_Click(object sender, RoutedEventArgs e)
       {
-         var dbCon = DBConnection.Instance();
-         dbCon.Server = "209.106.201.103";
-         dbCon.DatabaseName = "group2";
-         dbCon.UserName = "dbstudent4";
-         dbCon.Password = "slimydrum98";
-         if (dbCon.IsConnect())
-         {
-            //suppose col0 and col1 are defined as VARCHAR in the DB
-            string query = "SELECT col0,col1 FROM YourTable";
-            var cmd = new MySql.Data.MySqlClient.MySqlCommand(query, dbCon.Connection);
-            var reader = cmd.ExecuteReader();
-            while (reader.Read())
-            {
-               string someStringFromColumnZero = reader.GetString(0);
-               string someStringFromColumnOne = reader.GetString(1);
-               Console.WriteLine(someStringFromColumnZero + "," + someStringFromColumnOne);
-            }
-            dbCon.Close();
-         }
+         Window lastWindow = this;
+         FindGames findGames = new FindGames(lastWindow);
+         this.Hide();
+         findGames.ShowDialog();
+         //this.Visibility = Visibility.Collapsed;
+         findGames.Show();
+
       }
    }
 }
---
+++ GameDatabase/Preselected.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GameDatabase
+{
+   /// <summary>
+   /// Interaction logic for Preselected.xaml
+   /// </summary>
+   public partial class Preselected : Window
+   {
+      public Preselected()
+      {
+         InitializeComponent();
+      }
+
+      private void trendingGames_Click(object sender, RoutedEventArgs e)
+      {
+
+      }
+
+      private void newAdditions_Click(object sender, RoutedEventArgs e)
+      {
+
+      }
+   }
+}
